fcaacfdd1dde22dee4122d19ac65010c65b38ad7
twilson-gl <61714585+twilson-gl@users.noreply.github.com>
2021-06-14 13:51:17 -0700

    fixed issue with parameter type $Inheritance

--- WebrootUnity/Public/Console/GSM/Endpoint Management/Set-WebrootConsoleGSMEndpointGroup.ps1
+++ WebrootUnity/Public/Console/GSM/Endpoint Management/Set-WebrootConsoleGSMEndpointGroup.ps1
@@ -10,22 +10,18 @@ function Set-WebrootConsoleGSMEndpointGroup {
         [string]$SourceGroupId,
         [Parameter(Mandatory=$True)]
         [string]$TargetGroupId,
-        [ValidateSet("Unchanged","All","Update")]
-        [string]$Inheritance = "Unchanged"
+        [ValidateSet(0,1)]
+        [int]$Inheritance = 0
     )
 
     $url = "https://unityapi.webrootcloudav.com/service/api/console/gsm/$($GSMKey)/sites/$($SiteID)/endpoints"
 
-    switch ($Inheritance){
-        'Unchanged'{$Inheritance = 0}
-        'All'{$Inheritance = 1}
-        'Update'{$Inheritance = 3}
+    $Body = @{
+        EndpointsList = $EndpointsList
+        SourceGroupId = $SourceGroupId
+        TargetGroupId = $TargetGroupId
+        Inheritance   = $Inheritance
     }
-
-    $Body = @{EndpointsList=$EndpointsList;
-                SourceGroupId=$SourceGroupId;
-                TargetGroupId=$TargetGroupId;
-                Inheritance=$Inheritance;}
     $Body = $Body | ConvertTo-Json
 
     if ($PSCmdlet.ShouldProcess($WebRequestArguments.URI, "Invoke-RestMethod, with body:`r`n$Body`r`n")) {
@@ -36,7 +32,7 @@ function Set-WebrootConsoleGSMEndpointGroup {
             Invoke-RestMethod -Method Put -Uri $url -ContentType "application/json" -Body $Body -Headers @{"Authorization" = "Bearer $($WebrootAuthToken.access_token)"}
         }
         catch{
-            Write-Error "Error: $($Error[0])"
+            Write-Error "Error: $_.Exception.Message"
         }
     }
 }
